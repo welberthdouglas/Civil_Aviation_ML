de7b6f9f2882f2eeb7c4156653fb8f7c4eb098cd
welberthdouglas <welberthdouglas@gmail.com>
2020-04-07 01:25:02 -0300

    correção script.sh

--- script.sh
+++ script.sh
@@ -7,5 +7,5 @@ cat tmp1.txt  tmp2.txt > temp3.txt
 columns=$(<keep_columns.txt)
 csvcut -d ';' -c $columns -e 'ISO-8859-1' temp3.txt >  temp.csv
 csvgrep -c ds_servico_tipo_linha  -m PASSAGEIRO temp.csv | csvgrep -c ds_grupo_di -i -m IMPRODUTIVO | csvgrep -c ds_tipo_empresa -m 'TRANSPORTE AÉREO REGULAR' > data.csv
-#rm temp3.txt temp.csv
+rm temp3.txt temp.csv tmp1.txt tmp2.txt
 
